d17c30aaba0b75d8ff0a6dd632b3291af8bce31b
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2020-10-21 21:56:33 +1100

    spelling

--- src/lib/tests/timings.cpp
+++ src/lib/tests/timings.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <math.h>
 #include <chrono>
-#inlcude <memory.h>
+#include <memory.h>
 
 #include "distopia.h"  // a fancy approach
 #include "vanilla.h"  // a naive approach
