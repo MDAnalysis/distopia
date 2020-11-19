68e55627e9994f1d0364947ffd9d1313d0302d84
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2020-11-19 21:55:34 +1100

    fix up iterations in cmake macro

--- cmake/FindAVX.cmake
+++ cmake/FindAVX.cmake
@@ -2,6 +2,9 @@ INCLUDE(CheckCSourceRuns)
 INCLUDE(CheckCXXSourceRuns)
 
 SET(AVX_CODE "
+#if !defined __AVX__ // MSVC supports this flag since MSVS 2013
+#error \"__AVX__ define is missing\"
+#endif
 #include <immintrin.h>
 void test()
 {
@@ -10,6 +13,9 @@ void test()
 int main() { return 0; }")
 
 SET(AVX2_CODE "
+#if !defined __AVX2__ // MSVC supports this flag since MSVS 2013
+#error \"__AVX2__ define is missing\"
+#endif
 #include <immintrin.h>
 void test()
 {
@@ -17,6 +23,21 @@ void test()
     __m256i a = _mm256_loadu_si256((const __m256i *)data);
     __m256i b = _mm256_bslli_epi128(a, 1);  // available in GCC 4.9.3+
 }
+int main() { return 0; }")
+
+SET(AVX512_CODE "
+#if defined __AVX512__ || defined __AVX512F__
+#include <immintrin.h>
+void test()
+{
+    __m512i zmm = _mm512_setzero_si512();
+#if defined __GNUC__ && defined __x86_64__
+    asm volatile (\"\" : : : \"zmm16\", \"zmm17\", \"zmm18\", \"zmm19\");
+#endif
+}
+#else
+#error \"AVX512 is not supported\" 
+#endif
 int main() { return 0; }")
 
 
@@ -52,8 +73,10 @@ MACRO(CHECK_AVX lang type flags)
 
 ENDMACRO()
 
-CHECK_AVX(C "AVX" " ;-mavx;/arch:AVX")
-CHECK_AVX(C "AVX2" " ;-mavx2;/arch:AVX2")
+CHECK_AVX(C "AVX" "-mavx;/arch:AVX")
+CHECK_AVX(C "AVX2" "-mavx2;/arch:AVX2")
+CHECK_AVX(C "AVX512" "-mavx512;/arch:AVX512")
 
-CHECK_AVX(CXX "AVX" " ;-mavx;/arch:AVX")
-#CHECK_AVX(CXX "AVX2" " ;-mavx;/arch:AVX2")
+CHECK_AVX(CXX "AVX" "-mavx;/arch:AVX")
+CHECK_AVX(CXX "AVX2" "-mavx2;/arch:AVX2")
+CHECK_AVX(CXX "AVX512" "-mavx512;/arch:AVX512")
