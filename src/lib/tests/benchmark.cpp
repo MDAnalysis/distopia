109523193f6c7a84a350c577bc7c737da337cb94
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2021-01-14 15:08:45 +1100

    show initial application of googlebench

---
+++ src/lib/tests/benchmark.cpp
@@ -0,0 +1,69 @@
+#include <benchmark/benchmark.h>
+#include <iostream>
+#include <random>
+
+template <typename T>
+void RandomFloatingPoint(T *target, const std::size_t nrandom) {
+  std::random_device
+      rd; // Will be used to obtain a seed for the random number engine
+  std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
+  std::uniform_real_distribution<T> distribution(-10000, 10000);
+  for (size_t i = 0; i < nrandom; i++) {
+    target[i] = distribution(gen);
+  }
+}
+
+template <typename T> class CoordinatesDynamicMem : public benchmark::Fixture {
+public:
+  CoordinatesDynamicMem() : ncoords(0), coords(nullptr) {}
+
+  void SetUp(benchmark::State &state) {}
+  void InitCoords(const std::size_t ncoords) {
+    coords = new T[ncoords];
+    RandomFloatingPoint<T>(coords, ncoords);
+  }
+
+  void TearDown(benchmark::State &state) {
+
+    if (coords) {
+      std::free(coords);
+    }
+  }
+
+  void BMAccessModify(benchmark::State &state) {
+    ncoords = state.range(0);
+    InitCoords(ncoords);
+    for (auto _ : state) {
+      for (std::size_t i = 0; i < ncoords; i++) {
+        coords[i] += 0;
+      }
+    }
+    state.SetBytesProcessed(T(state.iterations()) * T(state.range(0)));
+  }
+
+  // members
+  std::size_t ncoords;
+  T *coords;
+};
+
+BENCHMARK_TEMPLATE_DEFINE_F(CoordinatesDynamicMem, ModifyFloats, float)
+(benchmark::State &state) {
+  BMAccessModify(state);
+}
+
+BENCHMARK_REGISTER_F(CoordinatesDynamicMem, ModifyFloats)
+    ->RangeMultiplier(4)
+    ->Range(16, 16 << 12);
+
+
+BENCHMARK_TEMPLATE_DEFINE_F(CoordinatesDynamicMem, ModifyDouble, double)
+(benchmark::State &state) {
+  BMAccessModify(state);
+}
+
+BENCHMARK_REGISTER_F(CoordinatesDynamicMem, ModifyDouble)
+    ->RangeMultiplier(4)
+    ->Range(16, 16 << 12);
+
+
+BENCHMARK_MAIN();
