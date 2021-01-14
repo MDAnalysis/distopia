54bc74513b77f1a8f66cc90fff2adacecc62c531
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2021-01-14 15:56:22 +1100

    clean up

--- src/lib/tests/benchmark.cpp
+++ src/lib/tests/benchmark.cpp
@@ -17,25 +17,25 @@ template <typename T> class CoordinatesDynamicMem : public benchmark::Fixture {
 public:
   CoordinatesDynamicMem() : ncoords(0), coords(nullptr) {}
 
-  void SetUp(benchmark::State &state) {}
-  void InitCoords(const std::size_t ncoords) {
+  void SetUp(benchmark::State &state) override {
+    ncoords = static_cast<std::size_t>(state.range(0));
+    InitCoords();
+  }
+  void InitCoords() {
     coords = new T[ncoords];
     RandomFloatingPoint<T>(coords, ncoords);
   }
 
-  void TearDown(benchmark::State &state) {
-
+  void TearDown(benchmark::State &state) override {
     if (coords) {
       std::free(coords);
     }
   }
 
   void BMAccessModify(benchmark::State &state) {
-    ncoords = state.range(0);
-    InitCoords(ncoords);
     for (auto _ : state) {
       for (std::size_t i = 0; i < ncoords; i++) {
-        coords[i] += 0;
+        coords[i] += 1.0;
       }
     }
     state.SetBytesProcessed(T(state.iterations()) * T(state.range(0)));
@@ -47,23 +47,17 @@ public:
 };
 
 BENCHMARK_TEMPLATE_DEFINE_F(CoordinatesDynamicMem, ModifyFloats, float)
-(benchmark::State &state) {
-  BMAccessModify(state);
-}
+(benchmark::State &state) { BMAccessModify(state); }
 
 BENCHMARK_REGISTER_F(CoordinatesDynamicMem, ModifyFloats)
     ->RangeMultiplier(4)
     ->Range(16, 16 << 12);
 
-
 BENCHMARK_TEMPLATE_DEFINE_F(CoordinatesDynamicMem, ModifyDouble, double)
-(benchmark::State &state) {
-  BMAccessModify(state);
-}
+(benchmark::State &state) { BMAccessModify(state); }
 
 BENCHMARK_REGISTER_F(CoordinatesDynamicMem, ModifyDouble)
     ->RangeMultiplier(4)
     ->Range(16, 16 << 12);
-
 
 BENCHMARK_MAIN();
