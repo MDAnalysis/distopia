5baf071e25fbc659c61046539b6e54affc68004d
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2021-01-16 23:27:48 +1100

    change swizzle tests to use datastructures

--- src/lib/include/datastructures.h
+++ src/lib/include/datastructures.h
@@ -4,21 +4,39 @@
 #include "distopia_type_traits.h"
 #include "x86_swizzle.h"
 #include "x86_vectors.h"
+#include "x86_tgintrin.h"
 
-template <typename SimdType, typename FloatType, EnableIfVector<SimdType> = 0,
-          EnableIfFloating<FloatType> = 0,
-          EnableIfMatching<SimdType, FloatType> = 0>
+template <typename VectorT, typename ScalarT, EnableIfVector<VectorT> = 0,
+          EnableIfFloating<ScalarT> = 0, EnableIfMatching<VectorT, ScalarT> = 0>
 class VectorTriple {
 public:
-  SimdType a;
-  SimdType b;
-  SimdType c;
-  inline VectorTriple(SimdType a, SimdType b, SimdType c) : a(a), b(b), c(c) {}
+  VectorT a;
+  VectorT b;
+  VectorT c;
+  // should this be size_t or uintptr_t?
+  constexpr static intptr_t nvals_per_pack = ValuesPerPack<VectorT>::value_p;
 
-  inline VectorTriple<SimdType, FloatType> Deinterleave() {
-    SimdType a1, b1, c1;
+  // from 3 SIMD Vector datatypes eg __m128 or __m128d
+  inline VectorTriple(VectorT a, VectorT b, VectorT c) : a(a), b(b), c(c) {}
+
+  // load from a vector of ScalarT eg float* or double *
+  inline VectorTriple(ScalarT *source) {
+    a = load_p(source);
+    b = load_p(source[nvals_per_pack]);
+    c = load_p(source[2 * nvals_per_pack]);
+  }
+
+    // to a vector of ScalarT eg float* or double *
+  inline void Store(ScalarT *target) {
+    target = store_p(a);
+    target[nvals_per_pack] = store_p(b);
+    target[2*nvals_per_pack] = store_p(c);
+  }
+  // AOS2SOA
+  inline VectorTriple<VectorT, ScalarT> Deinterleave() {
+    VectorT a1, b1, c1;
     Deinterleave3(a, b, c, a1, b1, c1);
-    VectorTriple<SimdType, FloatType> vt(a1, b1, c1);
+    VectorTriple<VectorT, ScalarT> vt(a1, b1, c1);
     return vt;
   }
 };
