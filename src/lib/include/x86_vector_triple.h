b792e081e745cf1f25380fe71e9e1cadfbab307d
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2021-01-17 20:13:32 +1100

    remove EnableIfMatching and EnableIfFloating SFINAE type checks

--- src/lib/include/x86_vector_triple.h
+++ src/lib/include/x86_vector_triple.h
@@ -7,26 +7,27 @@
 #include "x86_vectors.h"
 
 // forward declaration defines default EnableIfX template arguments
-template <typename VectorT, typename ScalarT, EnableIfVector<VectorT> = 0,
-          EnableIfFloating<ScalarT> = 0, EnableIfMatching<VectorT, ScalarT> = 0>
+template <typename VectorT, EnableIfVector<VectorT> = 0>
 class InterleavedVectorTriple;
 
 // forward declaration defines default EnableIfX template arguments
-template <typename VectorT, typename ScalarT, EnableIfVector<VectorT> = 0,
-          EnableIfFloating<ScalarT> = 0, EnableIfMatching<VectorT, ScalarT> = 0>
+template <typename VectorT, EnableIfVector<VectorT> = 0>
 class DeinterleavedVectorTriple;
 
-template <typename VectorT, typename ScalarT, EnableIfVector<VectorT> = 0,
-          EnableIfFloating<ScalarT> = 0, EnableIfMatching<VectorT, ScalarT> = 0>
+// VectorTriple base class
+template <typename VectorT, EnableIfVector<VectorT> = 0>
 class VectorTriple {
 public:
+  // maps to vectorT to ScalarT
+  using ScalarT = VectorToScalarT<VectorT>;
   VectorT a;
   VectorT b;
   VectorT c;
   constexpr static std::size_t nvals_per_pack = ValuesPerPack<VectorT>::value;
 
   // from 3 SIMD Vector datatypes eg __m128 or __m128d
-  inline explicit VectorTriple(VectorT a, VectorT b, VectorT c) : a(a), b(b), c(c) {}
+  inline explicit VectorTriple(VectorT a, VectorT b, VectorT c)
+      : a(a), b(b), c(c) {}
 
   // load from a vector of ScalarT eg float* or double * as constructor
   inline explicit VectorTriple(ScalarT *source) {
@@ -52,36 +53,34 @@ public:
 
 // This has been forward declared, so no longer allows EnableIfX = 0 default
 // declaration in same scope
-template <typename VectorT, typename ScalarT, EnableIfVector<VectorT>,
-          EnableIfFloating<ScalarT>, EnableIfMatching<VectorT, ScalarT>>
-class InterleavedVectorTriple : public VectorTriple<VectorT, ScalarT> {
+template <typename VectorT, EnableIfVector<VectorT>>
+class InterleavedVectorTriple : public VectorTriple<VectorT> {
 public:
-  //inherit both constructors with right SFINAE template arguments?
-  using VectorTriple<VectorT, ScalarT,0,0,0>::VectorTriple;
+  // inherit both constructors with right SFINAE template arguments?
+  using VectorTriple<VectorT,0>::VectorTriple;
 
   // AOS2SOA deinterleave
-  inline DeinterleavedVectorTriple<VectorT, ScalarT> deinterleave() {
+  inline DeinterleavedVectorTriple<VectorT> deinterleave() {
     VectorT a1, b1, c1;
     Deinterleave3(this->a, this->b, this->c, a1, b1, c1);
-    DeinterleavedVectorTriple<VectorT, ScalarT> vt(a1, b1, c1);
+    DeinterleavedVectorTriple<VectorT> vt(a1, b1, c1);
     return vt;
   }
 };
 
-//This has been forward declared, so no longer allows EnableIfX = 0 default
-//declaration in same scope
-template <typename VectorT, typename ScalarT, EnableIfVector<VectorT>,
-          EnableIfFloating<ScalarT>, EnableIfMatching<VectorT, ScalarT>>
-class DeinterleavedVectorTriple : public VectorTriple<VectorT, ScalarT> {
+// This has been forward declared, so no longer allows EnableIfX = 0 default
+// declaration in same scope
+template <typename VectorT, EnableIfVector<VectorT>>
+class DeinterleavedVectorTriple : public VectorTriple<VectorT> {
 public:
   // inherit both constructors with right SFINAE template arguments?
-  using VectorTriple<VectorT, ScalarT,0,0,0>::VectorTriple;
+  using VectorTriple<VectorT,0>::VectorTriple;
 
   // SOA2AOS interleave
-  inline InterleavedVectorTriple<VectorT, ScalarT> interleave() {
+  inline InterleavedVectorTriple<VectorT> interleave() {
     VectorT a1, b1, c1;
     Interleave3(this->a, this->b, this->c, a1, b1, c1);
-    InterleavedVectorTriple<VectorT, ScalarT> vt(a1, b1, c1);
+    InterleavedVectorTriple<VectorT> vt(a1, b1, c1);
     return vt;
   }
 };
