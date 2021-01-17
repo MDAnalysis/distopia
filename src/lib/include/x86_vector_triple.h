5075dc0a8127cd1a70a49390d1e3b1871acd817f
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2021-01-17 21:14:42 +1100

    add docs

--- src/lib/include/x86_vector_triple.h
+++ src/lib/include/x86_vector_triple.h
@@ -6,45 +6,54 @@
 #include "x86_tgintrin.h"
 #include "x86_vectors.h"
 
-// forward declaration defines default EnableIfX template arguments
+// forward declaration has default EnableIfX template arguments.
+// So no longer allows EnableIfX = 0 default in definition.
 template <typename VectorT, EnableIfVector<VectorT> = 0>
 class InterleavedVectorTriple;
 
-// forward declaration defines default EnableIfX template arguments
+
+// forward declaration has default EnableIfX template arguments.
+// So no longer allows EnableIfX = 0 default in definition.
 template <typename VectorT, EnableIfVector<VectorT> = 0>
 class DeinterleavedVectorTriple;
 
-// VectorTriple base class
+// VectorTriple base class packs 3xSimd datatypes into a single class.
+// Can be constructed from 3 x VectorT.
+// Can also be constructed from a ScalarT array which loads the right number
+// of values into the 3 x VectorT.
 template <typename VectorT, EnableIfVector<VectorT> = 0> class VectorTriple {
 public:
   // maps to vectorT to ScalarT
   using ScalarT = VectorToScalarT<VectorT>;
+  // 3x Simd Datatypes
   VectorT a;
   VectorT b;
   VectorT c;
+  // number of values in each VectorT datatype.
   constexpr static std::size_t nvals_per_pack = ValuesPerPack<VectorT>::value;
+  // number of values in the packed into the whole 3 x VectorT struct.
   constexpr static std::size_t nvals_per_struct = ValuesPerPack<VectorT>::value*3;
 
 
-  // from 3 SIMD Vector datatypes eg __m128 or __m128d
+  // construct from 3 SIMD Vector datatypes eg __m128 or __m128d
   inline explicit VectorTriple(VectorT a, VectorT b, VectorT c)
       : a(a), b(b), c(c) {}
 
-  // load from a vector of ScalarT eg float* or double * as constructor
+  // construct by loading from an array of ScalarT eg float* or double *.
   inline explicit VectorTriple(ScalarT *source) {
     a = loadu_p<VectorT>(source);
     b = loadu_p<VectorT>(source + nvals_per_pack);
     c = loadu_p<VectorT>(source + 2 * nvals_per_pack);
   }
 
-  // load from a vector of ScalarT eg float* or double *
+  // reload values from a array of ScalarT eg float* or double *.
   inline void load(ScalarT *source) {
     a = loadu_p<VectorT>(source);
     b = loadu_p<VectorT>(source + nvals_per_pack);
     c = loadu_p<VectorT>(source + 2 * nvals_per_pack);
   }
 
-  // to a vector of ScalarT eg float* or double *
+  // store to an array of ScalarT eg float* or double *.
   inline void store(ScalarT *target) {
     storeu_p(target, a);
     storeu_p(target + nvals_per_pack, b);
@@ -52,15 +61,15 @@ public:
   }
 };
 
-// This has been forward declared, so no longer allows EnableIfX = 0 default
-// declaration in same scope
+// Derived class for interleaved (AOS) vector triples. Can create a
+// deinterleaved (SOA) equivalent by calling deinterleave method.
 template <typename VectorT, EnableIfVector<VectorT>>
 class InterleavedVectorTriple : public VectorTriple<VectorT> {
 public:
-  // inherit both constructors with right SFINAE template argument
+  // inherit both constructors with right SFINAE template argument.
   using VectorTriple<VectorT, 0>::VectorTriple;
 
-  // AOS2SOA deinterleave
+  // AOS2SOA deinterleave, returns deinterleaved derived class.
   inline DeinterleavedVectorTriple<VectorT> deinterleave() {
     VectorT a1, b1, c1;
     Deinterleave3(this->a, this->b, this->c, a1, b1, c1);
@@ -69,15 +78,16 @@ public:
   }
 };
 
-// This has been forward declared, so no longer allows EnableIfX = 0 default
-// declaration in same scope
+// Derived class for Deinterleaved (SOA) vector triples. Can create a
+// interleaved (AOS) equivalent by calling interleave method.
+// TODO Interleave is not working or even a valid template.
 template <typename VectorT, EnableIfVector<VectorT>>
 class DeinterleavedVectorTriple : public VectorTriple<VectorT> {
 public:
-  // inherit both constructors with right SFINAE template argument
+  // inherit both constructors with right SFINAE template argument.
   using VectorTriple<VectorT, 0>::VectorTriple;
 
-  // SOA2AOS interleave
+  // SOA2AOS interleave returns interleaved derived class.
   inline InterleavedVectorTriple<VectorT> interleave() {
     VectorT a1, b1, c1;
     Interleave3(this->a, this->b, this->c, a1, b1, c1);
