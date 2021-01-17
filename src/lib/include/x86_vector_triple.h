ce2f4f555042f4e0e125a7f7d59c96fe5f2ab2ce
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2021-01-17 14:35:19 +1100

    inherit construtors

--- src/lib/include/x86_vector_triple.h
+++ src/lib/include/x86_vector_triple.h
@@ -3,8 +3,18 @@
 
 #include "distopia_type_traits.h"
 #include "x86_swizzle.h"
-#include "x86_vectors.h"
 #include "x86_tgintrin.h"
+#include "x86_vectors.h"
+
+// forward declaration defines default EnableIfX template arguments
+template <typename VectorT, typename ScalarT, EnableIfVector<VectorT> = 0,
+          EnableIfFloating<ScalarT> = 0, EnableIfMatching<VectorT, ScalarT> = 0>
+class InterleavedVectorTriple;
+
+// forward declaration defines default EnableIfX template arguments
+template <typename VectorT, typename ScalarT, EnableIfVector<VectorT> = 0,
+          EnableIfFloating<ScalarT> = 0, EnableIfMatching<VectorT, ScalarT> = 0>
+class DeinterleavedVectorTriple;
 
 template <typename VectorT, typename ScalarT, EnableIfVector<VectorT> = 0,
           EnableIfFloating<ScalarT> = 0, EnableIfMatching<VectorT, ScalarT> = 0>
@@ -16,10 +26,10 @@ public:
   constexpr static std::size_t nvals_per_pack = ValuesPerPack<VectorT>::value;
 
   // from 3 SIMD Vector datatypes eg __m128 or __m128d
-  inline VectorTriple(VectorT a, VectorT b, VectorT c) : a(a), b(b), c(c) {}
+  inline explicit VectorTriple(VectorT a, VectorT b, VectorT c) : a(a), b(b), c(c) {}
 
   // load from a vector of ScalarT eg float* or double * as constructor
-  inline VectorTriple(ScalarT *source) {
+  inline explicit VectorTriple(ScalarT *source) {
     a = load_p<VectorT>(source);
     b = load_p<VectorT>(source[nvals_per_pack]);
     c = load_p<VectorT>(source[2 * nvals_per_pack]);
@@ -34,15 +44,44 @@ public:
 
   // to a vector of ScalarT eg float* or double *
   inline void store(ScalarT *target) {
-    store_p<VectorT>(target, a);
-    store_p<VectorT>(target[nvals_per_pack], b);
-    store_p<VectorT>(target[2*nvals_per_pack], c);
+    store_p(target, a);
+    store_p(target[nvals_per_pack], b);
+    store_p(target[2 * nvals_per_pack], c);
   }
-  // AOS2SOA
-  inline VectorTriple<VectorT, ScalarT> deinterleave() {
+};
+
+// This has been forward declared, so no longer allows EnableIfX = 0 default
+// declaration in same scope
+template <typename VectorT, typename ScalarT, EnableIfVector<VectorT>,
+          EnableIfFloating<ScalarT>, EnableIfMatching<VectorT, ScalarT>>
+class InterleavedVectorTriple : public VectorTriple<VectorT, ScalarT> {
+public:
+  //inherit both constructors with right SFINAE template arguments?
+  using VectorTriple<VectorT, ScalarT,0,0,0>::VectorTriple;
+
+  // AOS2SOA deinterleave
+  inline DeinterleavedVectorTriple<VectorT, ScalarT> deinterleave() {
     VectorT a1, b1, c1;
-    Deinterleave3(a, b, c, a1, b1, c1);
-    VectorTriple<VectorT, ScalarT> vt(a1, b1, c1);
+    Deinterleave3(this->a, this->b, this->c, a1, b1, c1);
+    DeinterleavedVectorTriple<VectorT, ScalarT> vt(a1, b1, c1);
+    return vt;
+  }
+};
+
+//This has been forward declared, so no longer allows EnableIfX = 0 default
+//declaration in same scope
+template <typename VectorT, typename ScalarT, EnableIfVector<VectorT>,
+          EnableIfFloating<ScalarT>, EnableIfMatching<VectorT, ScalarT>>
+class DeinterleavedVectorTriple : public VectorTriple<VectorT, ScalarT> {
+public:
+  // inherit both constructors with right SFINAE template arguments?
+  using VectorTriple<VectorT, ScalarT,0,0,0>::VectorTriple;
+
+  // SOA2AOS interleave
+  inline InterleavedVectorTriple<VectorT, ScalarT> interleave() {
+    VectorT a1, b1, c1;
+    Interleave3(this->a, this->b, this->c, a1, b1, c1);
+    InterleavedVectorTriple<VectorT, ScalarT> vt(a1, b1, c1);
     return vt;
   }
 };
