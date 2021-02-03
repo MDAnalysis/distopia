397048e7152915a15a25ccbdbb17a58c43de8283
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2021-02-03 22:52:41 +1100

    finalise first kernel tests

--- src/lib/tests/test_kernels.cpp
+++ src/lib/tests/test_kernels.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
+#include <cassert>
 #include <iostream>
 #include <random>
-#include <cassert>
 
 #include "arrops.h"  //  fancy approaches
 #include "vanilla.h" // a naive approach
@@ -10,12 +10,13 @@
 #define BOXSIZE 10
 #define NCOORDS 3000
 
+// creates nrandom floating points between 0 and limit
 template <typename T>
-void RandomFloatingPoint(T *target, const std::size_t nrandom) {
+void RandomFloatingPoint(T *target, const int nrandom, const int limit) {
   std::random_device
       rd; // Will be used to obtain a seed for the random number engine
   std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
-  std::uniform_real_distribution<T> distribution(0, BOXSIZE - 1);
+  std::uniform_real_distribution<T> distribution(0, limit);
   for (size_t i = 0; i < nrandom; i++) {
     target[i] = distribution(gen);
   }
@@ -31,18 +32,25 @@ protected:
   T *coords1 = nullptr;
   T *ref = nullptr;
   T *results = nullptr;
-  T box[3] = {BOXSIZE, BOXSIZE, BOXSIZE};
+  T box[3];
 
-  void InitCoords(const int n) {
+  void InitCoords(const int n, const double boxsize) {
+
     ncoords = n;
     assert(n % 3 == 0);
     nresults = n / 3;
+
     coords0 = new T[ncoords];
     coords1 = new T[ncoords];
     ref = new T[nresults];
     results = new T[nresults];
-    RandomFloatingPoint<T>(coords0, ncoords);
-    RandomFloatingPoint<T>(coords1, ncoords);
+
+    RandomFloatingPoint<T>(coords0, ncoords, boxsize);
+    RandomFloatingPoint<T>(coords1, ncoords, boxsize);
+
+    box[0] = boxsize;
+    box[1] = boxsize;
+    box[2] = boxsize;
   }
 
   void TearDown() override {
@@ -66,8 +74,8 @@ using FloatTypes = ::testing::Types<float, double>;
 
 TYPED_TEST_SUITE(Coordinates, FloatTypes);
 
-TYPED_TEST(Coordinates, MatchesVanilla) {
-  this->InitCoords(NCOORDS);
+TYPED_TEST(Coordinates, CalcBondsMatchesVanilla) {
+  this->InitCoords(NCOORDS, BOXSIZE);
   VanillaCalcBonds<TypeParam>(this->coords0, this->coords1, this->box,
                               this->nresults, this->ref);
   CalcBondsOrtho(this->coords0, this->coords1, this->box, this->nresults,
