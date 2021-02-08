ae202df0e003008e92dc7ddab9e68c7dc2229ffe
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2021-02-08 21:42:12 +1100

    pull out ulps calculation

--- src/lib/tests/test_kernels.cpp
+++ src/lib/tests/test_kernels.cpp
@@ -10,69 +10,7 @@
 #define BOXSIZE 10
 #define NRESULTS 3000
 
-int32_t ulpsDistance(const float a, const float b) {
-  // Save work if the floats are equal.
-  // Also handles +0 == -0
-  if (a == b)
-    return 0;
-
-  const auto max = std::numeric_limits<int32_t>::max();
-
-  // Max distance for NaN
-  if (std::isnan(a) || std::isnan(b))
-    return max;
-
-  // If one's infinite and they're not equal, max distance.
-  if (std::isinf(a) || std::isinf(b))
-    return max;
-
-  int32_t ia, ib;
-  memcpy(&ia, &a, sizeof(float));
-  memcpy(&ib, &b, sizeof(float));
-
-  // Don't compare differently-signed floats.
-  if ((ia < 0) != (ib < 0))
-    return max;
-
-  // Return the absolute value of the distance in ULPs.
-  int32_t distance = ia - ib;
-  if (distance < 0)
-    distance = -distance;
-  return distance;
-}
-
-int64_t ulpsDistance(const double a, const double b) {
-  // Save work if the floats are equal.
-  // Also handles +0 == -0
-  if (a == b)
-    return 0;
-
-  const auto max = std::numeric_limits<int64_t>::max();
-
-  // Max distance for NaN
-  if (std::isnan(a) || std::isnan(b))
-    return max;
-
-  // If one's infinite and they're not equal, max distance.
-  if (std::isinf(a) || std::isinf(b))
-    return max;
-
-  int64_t ia, ib;
-  memcpy(&ia, &a, sizeof(double));
-  memcpy(&ib, &b, sizeof(double));
-
-  // Don't compare differently-signed floats.
-  if ((ia < 0) != (ib < 0))
-    return max;
-
-  // Return the absolute value of the distance in ULPs.
-  int64_t distance = ia - ib;
-  if (distance < 0)
-    distance = -distance;
-  return distance;
-}
-
- inline void EXPECT_EQ_T(float result, float ref) {
+inline void EXPECT_EQ_T(float result, float ref) {
   EXPECT_FLOAT_EQ(result, ref);
 }
 
@@ -84,9 +22,8 @@ inline void EXPECT_EQ_T(double result, double ref) {
 template <typename T>
 void RandomFloatingPoint(T *target, const int nrandom, const int neglimit,
                          const int poslimit) {
-            std::random_device
-      rd;               
-  std::mt19937 gen(rd()); // Standard mersenne_twister_engine 
+  std::random_device rd;
+  std::mt19937 gen(rd()); // Standard mersenne_twister_engine
   std::uniform_real_distribution<T> distribution(neglimit, poslimit);
   for (size_t i = 0; i < nrandom; i++) {
     target[i] = distribution(gen);
@@ -106,10 +43,11 @@ protected:
   T box[3];
 
   // coordinates range from 0 - delta to BOXSIZE + delta
-  void InitCoords(const int n_results, const double boxsize, const double delta) {
+  void InitCoords(const int n_results, const double boxsize,
+                  const double delta) {
 
     nresults = n_results;
-    ncoords = 3 * nresults ;
+    ncoords = 3 * nresults;
 
     coords0 = new T[ncoords];
     coords1 = new T[ncoords];
@@ -145,28 +83,19 @@ using FloatTypes = ::testing::Types<float, double>;
 
 TYPED_TEST_SUITE(Coordinates, FloatTypes);
 
-// coordinates in this test can overhang the edge of the box by 2 * the box size.
+// coordinates in this test can overhang the edge of the box by 2 * the box
+// size.
 TYPED_TEST(Coordinates, CalcBondsMatchesVanilla) {
   this->InitCoords(NRESULTS, BOXSIZE, 2 * BOXSIZE);
   VanillaCalcBonds<TypeParam>(this->coords0, this->coords1, this->box,
                               this->nresults, this->ref);
   CalcBondsOrtho(this->coords0, this->coords1, this->box, this->nresults,
                  this->results);
-  std::vector<int> ulps;
-  ulps.reserve(this->nresults);
 
   for (std::size_t i = 0; i < this->nresults; i++) {
-    auto ulp = ulpsDistance(this->results[i], this->ref[i]);
-    ulps.push_back(ulp);
     EXPECT_EQ_T(this->results[i], this->ref[i]);
     // loss of accuracy somewhere?
   }
-  // figure out how far away we are
-  auto max = *std::max_element(std::begin(ulps), std::end(ulps));
-  std::cout << " Max ulp deviation is " << max << " ULPS\n";
-  double sum = std::accumulate(std::begin(ulps), std::end(ulps), 0.0);
-  double mean = sum / ulps.size();
-  std::cout << " Average ulp deviation is " << mean << " ULPS\n";
 }
 
 // all the coordinates in this test are in the primary box
@@ -176,18 +105,7 @@ TYPED_TEST(Coordinates, CalcBondsMatchesVanillaInBox) {
                               this->nresults, this->ref);
   CalcBondsOrtho(this->coords0, this->coords1, this->box, this->nresults,
                  this->results);
-  std::vector<int> ulps;
-  ulps.reserve(this->nresults);
-
   for (std::size_t i = 0; i < this->nresults; i++) {
-    auto ulp = ulpsDistance(this->results[i], this->ref[i]);
-    ulps.push_back(ulp);
     EXPECT_EQ_T(this->results[i], this->ref[i]);
   }
-  // figure out how far away we are
-  auto max = *std::max_element(std::begin(ulps), std::end(ulps));
-  std::cout << " Max ulp deviation is " << max << " ULPS\n";
-  double sum = std::accumulate(std::begin(ulps), std::end(ulps), 0.0);
-  double mean = sum / ulps.size();
-  std::cout << " Average ulp deviation is " << mean << " ULPS\n";
 }
