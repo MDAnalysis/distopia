460042886bdf23af6cf626cf8fdbae94bd555a83
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2021-04-26 12:59:22 +1000

    add cross product

--- src/lib/include/ortho_box.h
+++ src/lib/include/ortho_box.h
@@ -10,6 +10,38 @@
 #include "ops.h"
 #include "vector_triple.h"
 
+// TODO remove this ugly shim when we have an atan2 vectorized implementation
+template <typename VectorT, EnableIfVector<VectorT> = 0>
+inline VectorT Atan2Shim(VectorT y, VectorT x) {
+  // unpack
+  VectorToScalarT<VectorT> y_buf[ValuesPerPack<VectorT>];
+  VectorToScalarT<VectorT> x_buf[ValuesPerPack<VectorT>];
+  VectorToScalarT<VectorT> res_buf[ValuesPerPack<VectorT>];
+  storeu_p(y_buf, y);
+  storeu_p(x_buf, x);
+  for (std::size_t i = 0; i < ValuesPerPack<VectorT>; i++) {
+    res_buf[i] = atan2(y_buf[i], x_buf[i]);
+  }
+  // re pack
+  VectorT result = loadu_p<VectorT>(res_buf);
+  return result;
+}
+
+template <typename VectorT, EnableIfFloating<VectorT> = 0>
+inline VectorT Atan2Shim(VectorT y, VectorT x) {
+  return atan2(y, x);
+}
+
+template <typename VectorT>
+inline VectorTriple<VectorT> CrossProduct(VectorTriple<VectorT> v0,
+                                          VectorTriple<VectorT> v1) {
+  VectorT r0 = v0.y * v1.z - v0.z * v1.y;
+  VectorT r1 = -v0.x * v1.z + v0.z * v1.x;
+  VectorT r2 = v0.x * v1.y - v0.y * v1.x;
+  auto result = VectorTriple<VectorT>(r0, r1, r2);
+  return result;
+}
+
 template <typename VectorT> class OrthogonalBox {
 public:
   using ScalarT = VectorToScalarT<VectorT>;
@@ -90,11 +122,15 @@ inline VectorT Angle3DWithBoundary(const VectorTriple<VectorT> &p1,
                                    const VectorTriple<VectorT> &p2,
                                    const VectorTriple<VectorT> &p3,
                                    const NoBox<VectorT> &) {
-  VectorTriple<VectorT> delta = p2 - p1;
-  VectorTriple<VectorT> r2 = delta * delta;
-  VectorT r = r2.x + r2.y + r2.z;
-
-  return Sqrt(r);
+  VectorTriple<VectorT> rji = p1 - p2;
+  VectorTriple<VectorT> rjk = p3 - p2;
+  VectorTriple<VectorT> x_acc = rji * rjk;
+  VectorT x = x_acc.x + x_acc.y + x_acc.z;
+  VectorTriple<VectorT> xp = CrossProduct<VectorT>(rji, rjk);
+  xp = xp * xp;
+  VectorT y_acc = xp.x + xp.y + xp.z;
+  VectorT y = Sqrt(y_acc);
+  return Atan2Shim(y, x);
 }
 
 #endif // DISTOPIA_ORTHO_BOX_H
