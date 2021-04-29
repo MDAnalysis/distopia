f943c4db85297d3470b27375878d292240cc7281
hugo <hugomacdermott@gmail.com>
2021-04-29 16:59:43 +1000

    add minimum image func

--- src/compare/vanilla.h
+++ src/compare/vanilla.h
@@ -7,6 +7,22 @@
 
 #include <cmath>
 #include <iostream>
+#include <float.h>
+
+
+template<typename T> //from mda
+void minimum_image(T *x, const T *box,
+                          const T *inverse_box) {
+  int i;
+  T s;
+  for (i = 0; i < 3; i++) {
+    if (box[i] > FLT_EPSILON) {
+      s = inverse_box[i] * x[i];
+      x[i] = box[i] * (s - round(s));
+    }
+  }
+}
+
 
 template <typename T>
 void VanillaCalcBonds(const T *coords1, const T *coords2, const T *box,
@@ -46,7 +62,7 @@ void VanillaCalcBondsIdx(const T *coords, std::size_t *idx, const T *box,
     T r2 = 0.0;
     for (unsigned char j = 0; j < 3; ++j) {
       T rij = coords[b1 * 3 + j] - coords[b2 * 3 + j];
-      T adj = round(rij / box[j]);
+      T adj = std::round(rij / box[j]);
       rij -= adj * box[j];
 
       r2 += rij * rij;
@@ -61,15 +77,20 @@ void VanillaCalcAngles(const T *coords1, const T *coords2, const T *coords3,
   T rji[3];
   T rjk[3];
   T xp[3];
+  T inverse_box[3];
+
+  inverse_box[0] = 1.0 / box[0];
+  inverse_box[1] = 1.0 / box[1];
+  inverse_box[2] = 1.0 / box[2];
+
   for (unsigned int i = 0; i < nvals; ++i) {
     for (unsigned char j = 0; j < 3; ++j) {
-      T temp = coords1[i * 3 + j] - coords2[i * 3 + j];
-      T adj = std::round(temp / box[j]);
-      rji[j] -= adj * box[j];
-      temp = coords3[i * 3 + j] - coords2[i * 3 + j];
-      adj = std::round(temp / box[j]);
-      rjk[j] -= adj * box[j];
+      rji[j] = coords1[i * 3 + j] - coords2[i * 3 + j];
+      rjk[j] = coords3[i * 3 + j] - coords2[i * 3 + j];
     }
+    minimum_image(rji, box, inverse_box);
+    minimum_image(rjk, box, inverse_box);
+
     T x = rji[0] * rjk[0] + rji[1] * rjk[1] + rji[2] * rjk[2];
 
     xp[0] = rji[1] * rjk[2] - rji[2] * rjk[1];
