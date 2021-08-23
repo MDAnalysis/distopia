679cdbbea31f65e0ed364655c5068b654fb786c7
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2021-08-23 23:55:42 +1000

    move stuff out of ortho_box

--- src/lib/include/ops.h
+++ src/lib/include/ops.h
@@ -5,12 +5,7 @@
 #include "vector_triple_basemath.h"
 #include "x86_vectors.h"
 
-namespace {
 
-template<typename T>
-T Distance1DWithBoundary(T p0, T p1, T b) {
-  return DistanceModulo(p0, p1, b);
-}
 
 template<typename T>
 T Hypot(T x, T y, T z) {
@@ -22,15 +17,37 @@ T Hypot(T x, T y, T z) {
   return norm;
 }
 
-template<typename T>
-T Distance3DWithBoundary(T x0, T y0, T z0, T x1, T y1, T z1,
-                         T bx, T by, T bz) {
-  T dx = Distance1DWithBoundary(x0, x1, bx);
-  T dy = Distance1DWithBoundary(y0, y1, by);
-  T dz = Distance1DWithBoundary(z0, z1, bz);
-  return Hypot(dx, dy, dz);
+// TODO remove this ugly shim when we have an atan2 vectorized implementation
+template <typename VectorT, EnableIfVector<VectorT> = 0>
+inline VectorT Atan2Shim(VectorT y, VectorT x) {
+  // unpack
+  VectorToScalarT<VectorT> y_buf[ValuesPerPack<VectorT>];
+  VectorToScalarT<VectorT> x_buf[ValuesPerPack<VectorT>];
+  VectorToScalarT<VectorT> res_buf[ValuesPerPack<VectorT>];
+  storeu_p(y_buf, y);
+  storeu_p(x_buf, x);
+  for (std::size_t i = 0; i < ValuesPerPack<VectorT>; i++) {
+    res_buf[i] = atan2(y_buf[i], x_buf[i]);
+  }
+  // re pack
+  VectorT result = loadu_p<VectorT>(res_buf);
+  return result;
 }
 
-} // namespace
+template <typename VectorT, EnableIfFloating<VectorT> = 0>
+inline VectorT Atan2Shim(VectorT y, VectorT x) {
+  return atan2(y, x);
+}
+
+template <typename VectorT>
+inline VectorTriple<VectorT> CrossProduct(VectorTriple<VectorT> v0,
+                                          VectorTriple<VectorT> v1) {
+  VectorT r0 = v0.y * v1.z - v0.z * v1.y;
+  VectorT r1 = v0.z * v1.x - v0.x * v1.z;
+  VectorT r2 = v0.x * v1.y - v0.y * v1.x;
+  auto result = VectorTriple<VectorT>(r0, r1, r2);
+  return result;
+}
+
 
 #endif // DISTOPIA_OPS_H
--- src/lib/include/ortho_box.h
+++ src/lib/include/ortho_box.h
@@ -10,38 +10,6 @@
 #include "ops.h"
 #include "vector_triple.h"
 
-// TODO remove this ugly shim when we have an atan2 vectorized implementation
-template <typename VectorT, EnableIfVector<VectorT> = 0>
-inline VectorT Atan2Shim(VectorT y, VectorT x) {
-  // unpack
-  VectorToScalarT<VectorT> y_buf[ValuesPerPack<VectorT>];
-  VectorToScalarT<VectorT> x_buf[ValuesPerPack<VectorT>];
-  VectorToScalarT<VectorT> res_buf[ValuesPerPack<VectorT>];
-  storeu_p(y_buf, y);
-  storeu_p(x_buf, x);
-  for (std::size_t i = 0; i < ValuesPerPack<VectorT>; i++) {
-    res_buf[i] = atan2(y_buf[i], x_buf[i]);
-  }
-  // re pack
-  VectorT result = loadu_p<VectorT>(res_buf);
-  return result;
-}
-
-template <typename VectorT, EnableIfFloating<VectorT> = 0>
-inline VectorT Atan2Shim(VectorT y, VectorT x) {
-  return atan2(y, x);
-}
-
-template <typename VectorT>
-inline VectorTriple<VectorT> CrossProduct(VectorTriple<VectorT> v0,
-                                          VectorTriple<VectorT> v1) {
-  VectorT r0 = v0.y * v1.z - v0.z * v1.y;
-  VectorT r1 = v0.z * v1.x - v0.x * v1.z;
-  VectorT r2 = v0.x * v1.y - v0.y * v1.x;
-  auto result = VectorTriple<VectorT>(r0, r1, r2);
-  return result;
-}
-
 template <typename VectorT> class OrthogonalBox {
 public:
   using ScalarT = VectorToScalarT<VectorT>;
