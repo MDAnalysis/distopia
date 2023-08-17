4efb98632da1f4a25f00adb2bf1e8d45c8c9f96a
richard gowers <richardjgowers@gmail.com>
2023-08-17 11:46:49 +0100

    partial loops working in calc_bonds

--- calc_bonds.cpp
+++ calc_bonds.cpp
@@ -21,7 +21,7 @@ namespace roadwarrior {
 
         template <class V>
         HWY_INLINE V distance(const V &ax, const V &ay, const V &az,
-                   const V &bx, const V &by, const V &bz) {
+                              const V &bx, const V &by, const V &bz) {
             auto dx = ax - bx;
             auto dy = ay - by;
             auto dz = az - bz;
@@ -41,7 +41,28 @@ namespace roadwarrior {
         template <typename T>
         void calc_bonds(const T* a, const T* b, int n, T* out) {
             const hn::ScalableTag<T> d;
-            int N = hn::Lanes(d);
+            int nlanes = hn::Lanes(d);
+
+            // temporary arrays used for problem sizes smaller than nlanes
+            T a_sub[3 * HWY_MAX_LANES_D(hn::ScalableTag<T>)];
+            T b_sub[3 * HWY_MAX_LANES_D(hn::ScalableTag<T>)];
+            T out_sub[HWY_MAX_LANES_D(hn::ScalableTag<T>)];
+            const T *a_src, *b_src;
+            T *dst;
+
+            if (HWY_UNLIKELY(n < nlanes)) {
+                // input problem was too small to bother with
+                memcpy(a_sub, a, 3 * n * sizeof(T));
+                memcpy(b_sub, b, 3 * n * sizeof(T));
+
+                a_src = &a_sub[0];
+                b_src = &b_sub[0];
+                dst = &out_sub[0];
+            } else {
+                a_src = &a[0];
+                b_src = &b[0];
+                dst = out;
+            }
 
             auto a_x = hn::Undefined(d);
             auto a_y = hn::Undefined(d);
@@ -50,23 +71,29 @@ namespace roadwarrior {
             auto b_y = hn::Undefined(d);
             auto b_z = hn::Undefined(d);
 
-            for (int i=0; i<n; i += N) {
-                hn::LoadInterleaved3(d, a + i, a_x, a_y, a_z);
-                hn::LoadInterleaved3(d, b + i, b_x, b_y, b_z);
+            for (int i=0; i<n; i += nlanes) {
+                // to deal with end of loop, can't load starting further than a[n - nlanes] so clip i to that
+                size_t p = HWY_MIN(i, n - nlanes);
 
-                hn::Print(d, "ax is: ", a_x, 0, 16);
-                hn::Print(d, "ay is: ", a_y, 0, 16);
-                hn::Print(d, "az is: ", a_z, 0, 16);
+                hn::LoadInterleaved3(d, a_src + 3 * p, a_x, a_y, a_z);
+                hn::LoadInterleaved3(d, b_src + 3 * p, b_x, b_y, b_z);
+
+                hn::Print(d, "ax is: ", a_x, 0, nlanes);
+                hn::Print(d, "ay is: ", a_y, 0, nlanes);
+                hn::Print(d, "az is: ", a_z, 0, nlanes);
                 std::cout << std::endl;
-                hn::Print(d, "bx is: ", b_x, 0, 16);
-                hn::Print(d, "by is: ", b_y, 0, 16);
-                hn::Print(d, "bz is: ", b_z, 0, 16);
+                hn::Print(d, "bx is: ", b_x, 0, nlanes);
+                hn::Print(d, "by is: ", b_y, 0, nlanes);
+                hn::Print(d, "bz is: ", b_z, 0, nlanes);
 
                 auto result = distance(a_x, a_y, a_z, b_x, b_y, b_z);
 
                 hn::Print(d, "result is: ", result, 0, 16);
 
-                hn::StoreU(result, d, out + i);
+                hn::StoreU(result, d, dst + p);
+            }
+            if (HWY_UNLIKELY(n < nlanes)) {
+                memcpy(out, dst, n * sizeof(T));
             }
         }
 
@@ -88,6 +115,8 @@ namespace roadwarrior {
     HWY_EXPORT(calc_bonds_single);
 
     HWY_DLLEXPORT template <> void calc_bonds(const float* a, const float* b, int n, float* out) {
+        // TODO: Could instead put small problem handling here, if n<16 manually dispatch to non-vector route
+        //       Would benefit the codepath in all vector versions
         return HWY_DYNAMIC_DISPATCH(calc_bonds_single)(a, b, n, out);
     }
     HWY_DLLEXPORT template <> void calc_bonds(const double* a, const double* b, int n, double* out) {
--- run_calc_bonds.cpp
+++ run_calc_bonds.cpp
@@ -8,23 +8,25 @@
 int main(int argc, char** argv) {
     std::cout << "prepare yourselves" << std::endl;
 
-    float a[3*32], b[3*32], c[32];
-    int n=32;
+    const size_t n_pos = 33;
+
+    float a[3*n_pos], b[3*n_pos], c[n_pos];
 
     // setup inputs
-    for (int i=0; i<32; ++i) {
+    for (int i=0; i<n_pos; ++i) {
         a[i*3] = i*3.;
         a[i*3 + 1] = i*3. + 1;
         a[i*3 + 2] = i*3. + 2;
 
-        b[i*3] = i*3. + 10.;
-        b[i*3 + 1] = i*3. + 1 + 10;
-        b[i*3 + 2] = i*3. + 2 + 10;
+        auto base = static_cast<float>((i*3) * (i*3));
+        b[i*3] = base;
+        b[i*3 + 1] = base;
+        b[i*3 + 2] = base;
 
         c[i] = 0.0f;
     }
 
-    roadwarrior::calc_bonds(a, b, n, c);
+    roadwarrior::calc_bonds(a, b, n_pos, c);
 
     for (float i : c) {
         std::cout << i << std::endl;
