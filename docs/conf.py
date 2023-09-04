193874f50dfd943db80a7ae9fc48bfd213a9c6c4
IAlibay <ialibay@mdanalysis.org>
2023-09-04 08:28:09 +0100

    Update for mda sphinx theme, update author names

--- docs/conf.py
+++ docs/conf.py
@@ -16,11 +16,9 @@ import distopia
 # sys.path.insert(0, os.path.abspath('../')
 
 # -- Project information -----------------------------------------------------
-import sphinx_rtd_theme
-
 project = 'distopia'
-copyright = '2022, Hugo MacDermott-Opeskin Jakub Nabaglo Richard Gowers'
-author = 'Hugo MacDermott-Opeskin Jakub Nabaglo Richard Gowers'
+copyright = '2022, Hugo MacDermott-Opeskin Jacqueline Nabaglo Richard Gowers'
+author = 'Hugo MacDermott-Opeskin Jacqueline Nabaglo Richard Gowers'
 
 
 # -- General configuration ---------------------------------------------------
@@ -57,25 +55,10 @@ pygments_style = 'default'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-
-html_theme_path = [
-    sphinx_rtd_theme.get_html_theme_path()
-]
+html_theme = 'mdanalysis_sphinx_theme'
 
 html_theme_options = {
-    'canonical_url': '',
-    'logo_only': True,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False,
+    'mda_official': True,
 }
 
 # options common to RTD and MDAnalysis theme
@@ -85,5 +68,4 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 # For RTD theme: custom.css to override theme defaults.
 html_static_path = ['_static']
-html_css_files = ['custom.css']
-html_logo = "_static/logos/mdanalysis-logo-thin.png"
+# html_css_files = []
