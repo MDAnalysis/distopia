108420b0ab63ddd35e4ae37112b66217ec8206a5
richard gowers <richardjgowers@gmail.com>
2023-09-17 11:16:29 +0100

    use cython floating fused types to resolve template
    reduces number of function declarations, boilerplate now lives in cython side

--- python/distopia/_distopia.pyx
+++ python/distopia/_distopia.pyx
@@ -13,6 +13,7 @@ The python functions for distopia
 import numpy as np
 cimport cython
 cimport numpy as cnp
+from cython cimport floating
 cnp.import_array()
 
 cdef extern from "distopia.h" namespace "distopia" nogil:
@@ -31,151 +32,86 @@ cdef extern from "distopia.h" namespace "distopia" nogil:
                            T * output)
 
 
-def NFloatLanes():
+def get_n_float_lanes():
     """The number of floats per register distopia will handle on this system"""
     return GetNFloatLanes()
 
 
-def NDoubleLanes():
+def get_n_double_lanes():
     """The number of doubles per register distopia will handle on this system"""
     return GetNDoubleLanes()
 
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cpdef cnp.ndarray[cnp.float32_t, ndim = 1] calc_bonds_ortho_float(float[:, ::1] coords0,
-                                                                 float[:, ::1] coords1,
-                                                                 float[::1] box,
-                                                                 cnp.ndarray results=None):
+def  calc_bonds_ortho(floating[:, ::1] coords0,
+                      floating[:, ::1] coords1,
+                      floating[::1] box,
+                      floating[::1] results=None):
     """Calculate pairwise distances between coords0 and coords1
 
     Parameters
     ----------
-    coords0, coords1 : float32 array
-      must be same length
-    box : float32 array 
+    coords0, coords1 : float32 or float64 array
+      must be same length and dtype
+    box : float32 or float64 array
       periodic boundary dimensions
-    results: float32 array (optional)
-      array to store results in, must be same size as coords0/coords1
+    results: float32 or float64 array (optional)
+      array to store results in, must be same length and dtype as coords0/coords1
 
     Returns
     -------
     distances : float32 array
       same size as coords0/coords1
     """
-    cdef float[::1] results_view
+    cdef floating[::1] results_view
     cdef size_t nvals = coords0.shape[0]
     cdef cnp.npy_intp[1] dims
     dims[0] = <ssize_t > nvals  # FIXME truncation?
     if results is None:
-        results = cnp.PyArray_EMPTY(1, dims, cnp.NPY_FLOAT32, 0)
+        if floating is float:
+            results = cnp.PyArray_EMPTY(1, dims, cnp.NPY_FLOAT32, 0)
+        else:
+            results = cnp.PyArray_EMPTY(1, dims, cnp.NPY_FLOAT64, 0)
 
     results_view = results
 
     CalcBondsOrtho(& coords0[0][0], & coords1[0][0], nvals, & box[0], & results_view[0])
 
-    return results
+    return np.array(results)
 
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cpdef cnp.ndarray[cnp.float64_t, ndim = 1] calc_bonds_ortho_double(double[:, ::1] coords0,
-                                                                  double[:, ::1] coords1,
-                                                                  double[::1] box,
-                                                                  cnp.ndarray results=None):
+def calc_bonds_no_box(floating[:, ::1] coords0,
+                      floating[:, ::1] coords1,
+                      results=None):
     """Calculate pairwise distances between coords0 and coords1
 
     Parameters
     ----------
-    coords0, coords1 : float64 array
-      must be same length
-    box : float64 array
-      periodic boundary dimensions
-    results: float64 array (optional)
-      array to store results in, must be same size as coords0/coords1
+    coords0, coords1 : float32 or float64 array
+      must be same length and same dtype
+    results: float32 of float 64 array (optional)
+      array to store results in, must be same size and dtype as coords0/coords1
 
     Returns
     -------
-    distances : float64 array
-      same size as coords0/coords1
+    distances : float32 or float64 array
+      same length and dtype as coords0/coords1
     """
-    cdef double[::1] results_view
+    cdef floating[::1] results_view
     cdef size_t nvals = coords0.shape[0]
     cdef cnp.npy_intp[1] dims
-    dims[0] = <ssize_t > nvals  # FIXME truncation?
+    dims[0] = <ssize_t> nvals  # FIXME truncation?
     if results is None:
-        results = cnp.PyArray_EMPTY(1, dims, cnp.NPY_FLOAT64, 0)
-
-    results_view = results
-
-    CalcBondsOrtho(& coords0[0][0], & coords1[0][0], nvals, & box[0], & results_view[0])
-
-    return results
-
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-cpdef cnp.ndarray[cnp.float32_t, ndim = 1] calc_bonds_no_box_float(float[:, ::1] coords0,
-                                                                  float[:, ::1] coords1,
-                                                                  cnp.ndarray results=None):
-    """Calculate pairwise distances between coords0 and coords1
-
-    Parameters
-    ----------
-    coords0, coords1 : float32 array
-      must be same length
-    results: float32 array (optional)
-      array to store results in, must be same size as coords0/coords1
-
-    Returns
-    -------
-    distances : float32 array
-      same size as coords0/coords1
-    """
-    cdef float[::1] results_view
-    cdef size_t nvals = coords0.shape[0]
-    cdef cnp.npy_intp[1] dims
-    dims[0] = <ssize_t > nvals  # FIXME truncation?
-    if results is None:
-        results = cnp.PyArray_EMPTY(1, dims, cnp.NPY_FLOAT32, 0)
+        if floating is float:
+            results = cnp.PyArray_EMPTY(1, dims, cnp.NPY_FLOAT32, 0)
+        else:
+            results = cnp.PyArray_EMPTY(1, dims, cnp.NPY_FLOAT64, 0)
 
     results_view = results
 
     CalcBondsNoBox(& coords0[0][0], & coords1[0][0], nvals, & results_view[0])
 
-    return results
-
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-cpdef cnp.ndarray[cnp.float64_t, ndim = 1] calc_bonds_no_box_double(double[:, ::1] coords0,
-                                                                   double[:, ::1] coords1,
-                                                                   cnp.ndarray results=None):
-    """Calculate pairwise distances between coords0 and coords1
-
-    Parameters
-    ----------
-    coords0, coords1 : float64 array
-      must be same length
-    box : float64 array
-      periodic boundary dimensions
-    results: float64 array (optional)
-      array to store results in, must be same size as coords0/coords1
-
-    Returns
-    -------
-    distances : float64 array
-      same size as coords0/coords1
-    """
-    cdef double[::1] results_view
-    cdef size_t nvals = coords0.shape[0]
-    cdef cnp.npy_intp[1] dims
-    dims[0] = <ssize_t > nvals  # FIXME truncation?
-    if results is None:
-        results = cnp.PyArray_EMPTY(1, dims, cnp.NPY_FLOAT64, 0)
-
-    results_view = results
-
-    CalcBondsNoBox(& coords0[0][0], & coords1[0][0], nvals, & results_view[0])
-
-    return results
+    return np.array(results)
