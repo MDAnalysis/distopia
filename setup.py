b7cff68c0278b2bd5bcf0b4bdd3878ecb924a951
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2023-10-09 22:01:21 +1100

    improve

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from skbuild import setup
-import versioneer
 import os
 
 description = "Fast distance calculations using explicitly vectorised SIMD"
@@ -12,39 +11,8 @@ except ImportError:
 
 setup(
     name="distopia",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    long_description_content_type = "text/markdown",
-    description=description,
-    long_description=long_description,
-    author=['Hugo MacDermott-Opeskin', "Richard Gowers"],
-    license="MIT",
     packages=['distopia'],
     python_requires=">=3.9",
-    keywords=(
-        "molecular dynamics distances simulation SIMD"
-    ),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: C++",
-        "Programming Language :: Cython",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Topic :: Scientific/Engineering :: Chemistry',
-    ],
-    url="https://github.com/MDAnalysis/distopia",
-    download_url="https://pypi.org/project/distopia/",
-    project_urls={
-        "Homepage": "https://github.com/MDAnalysis/distopia",
-        "Documentation": "https://www.mdanalysis.org/distopia/",
-        "Source Code": "https://github.com/MDAnalysis/distopia",
-        "Issue Tracker": "https://github.com/MDAnalysis/distopia/issues",
-    },
     install_requires=[
         "numpy>=1.20.0"
     ],
