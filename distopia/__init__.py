cdb8133cb6195c0aebea07c0abb596222216c04c
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2024-06-26 22:22:03 +1000

    fix

--- distopia/__init__.py
+++ distopia/__init__.py
@@ -3,10 +3,12 @@ from ._version import __version__
 from ._distopia import (
     calc_bonds_ortho,
     calc_bonds_no_box,
+    calc_bonds_triclinic
 )
 
 __all__ = [
     'calc_bonds_ortho',
     'calc_bonds_no_box',
+    'calc_bonds_triclinic',
     '__version__',
 ]
